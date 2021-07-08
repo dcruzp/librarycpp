b22c2580586d4f3cd15541df2b321aa7c58e7700
ddcruzp <david.delacruz9041@gmail.com>
2021-07-07 23:53:23 -0400

    Adding other solution for problem 200_B

--- 200_B_Drinks/drinks.cpp
+++ 200_B_Drinks/drinks.cpp
@@ -1,33 +1,73 @@
-#include <iostream> 
+// #include <iostream> 
 
-//La longitud maxima de los datos de entrada 
-#define MAX_LENGHT 100 
+// //La longitud maxima de los datos de entrada 
+// #define MAX_LENGHT 100 
 
-using namespace std ; 
+// using namespace std ; 
 
-//un array de longitud tan grande como pueda haber 
-//datos de entrada , una variable para n y otro para 
-// la sumatoria de los datos de entrada 
-float store [MAX_LENGHT];  
-int n , sum ; 
+// //un array de longitud tan grande como pueda haber 
+// //datos de entrada , una variable para n y otro para 
+// // la sumatoria de los datos de entrada 
+// float store [MAX_LENGHT];  
+// int n , sum ; 
 
-int main ()
+// int main ()
+// {
+//     //leo el valor de n para saber la cantidad de 
+//     //elementos que hay en la entrada
+//     cin >> n ;
+
+//     //pongo la variable sumatoria en cero y hago 
+//     //un for para leer y suma todos los datos de 
+//     //la entrada
+//     sum = 0 ;  
+//     for (size_t i = 0; i < n; i++)
+//     {
+//         cin >> store[i]; 
+//         sum += store [i]; 
+//     }
+    
+//     //imprimo la  suma de los datos entre la cantidad 
+//     //de datos para saber el promedio de los datos 
+//     cout << sum / (float)n  << endl ; 
+// }
+
+
+// ------------------------------------------------OTRA SOLUCION DEL PROBLEMA--------------------------------------------------------/
+
+#include<iostream>
+
+
+using namespace std;
+
+void porcentajes(float);
+float calcular( float a[] , float);
+float n;
+float a[100];
+
+int main()
 {
-    //leo el valor de n para saber la cantidad de 
-    //elementos que hay en la entrada
-    cin >> n ;
+    cin>>n;
+    porcentajes(n);
+    cout<<calcular(a,n)<<endl;
+    return 0;
+}
 
-    //pongo la variable sumatoria en cero y hago 
-    //un for para leer y suma todos los datos de 
-    //la entrada
-    sum = 0 ;  
-    for (size_t i = 0; i < n; i++)
+void porcentajes(float n)
+{
+    for (int i = 0; i < n; i++)
     {
-        cin >> store[i]; 
-        sum += store [i]; 
+        cin>>a[i];
     }
     
-    //imprimo la  suma de los datos entre la cantidad 
-    //de datos para saber el promedio de los datos 
-    cout << sum / (float)n  << endl ; 
+}
+
+float calcular( float a[] , float n)
+{  
+    float cont = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cont += a[i];
+    }
+    return cont / n;
 }
